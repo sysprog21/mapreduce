c2645dc9aa3cf8542c151cf8410dc4bb5d8ba668
Jim Huang <jserv.tw@gmail.com>
2017-03-11 23:51:51 +0800

    Eliminate repeated error checks

--- src/threadpool.c
+++ src/threadpool.c
@@ -101,12 +101,10 @@ threadpool_t *threadpool_create(int thread_count, int queue_size, int flags)
     int i;
     (void) flags;
 
-    if(thread_count <= 0 || thread_count > MAX_THREADS || queue_size <= 0 || queue_size > MAX_QUEUE) {
-        return NULL;
-    }
+    if(thread_count <= 0) return NULL;
 
-    if(thread_count <= 0 || thread_count > THREADS_MAX) {
-        goto err;
+    if(thread_count > MAX_THREADS || queue_size <= 0 || queue_size > MAX_QUEUE) {
+        return NULL;
     }
 
     if((pool = (threadpool_t *)malloc(sizeof(threadpool_t))) == NULL) {
